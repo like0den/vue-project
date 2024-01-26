39dbd5cbb53231421dfac1359a24b7b979877a5c
emura shigeto <shigeto@emuranoMacBook-Air.local>
2024-01-26 12:52:16 +0900

    v-model使用

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,71 @@
 <script setup>
 import { ref } from "vue";
 
-const msg = ref("Hello World!");
+const header = ref("Shopping List App");
+const items = ref([
+  { id: 1, label: "10 party hats" },
+  { id: 2, label: "2 boards games" },
+  { id: 3, label: "20 cups" },
+]);
+const newItem = ref("");
+const newItemPriority = ref("low");
+const newItemSelectBox = ref("low");
+const newItemCheckBox = ref(false);
+const iceCreamFlavors = ref([]);
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-  <input v-model="msg" />
-  <input v-model="msg" />
+  <h1>{{ header }}</h1>
+  <input v-model.lazy="newItem" type="text" placeholder="Add an item" />
+  <!-- ラジオボタン -->
+  Priority:
+  <label>
+    <input type="radio" v-model="newItemPriority" value="low" />
+    Low
+  </label>
+  <label>
+    <input type="radio" v-model="newItemPriority" value="high" />
+    High
+  </label>
+  <br />
+  <!-- セレクトボックス -->
+  <label>
+    Priority:
+    <select v-model="newItemSelectBox">
+      <option value="low">Low</option>
+      <option value="high">High</option>
+    </select>
+  </label>
+  <br />
+  <!-- チェックボックス -->
+  <label>
+    <input type="checkbox" v-model="newItemCheckBox" />
+    High Priority
+    {{ newItemCheckBox }}
+  </label>
+  <br />
+  <!-- リスト -->
+  <label>
+    <input type="checkbox" v-model="iceCreamFlavors" value="vanilla" />Vanilla
+  </label>
+  <label>
+    <input
+      type="checkbox"
+      v-model="iceCreamFlavors"
+      value="Chocolate"
+    />Chocolate
+  </label>
+  <label>
+    <input
+      type="checkbox"
+      v-model="iceCreamFlavors"
+      value="strawberry"
+    />Strawberry
+  </label>
+  <!-- リスト -->
+  <ul>
+    <li v-for="{ id, label } in items" :key="id">
+      {{ label }}
+    </li>
+  </ul>
 </template>
