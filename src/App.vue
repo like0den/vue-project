24c1f0c00d3215016b012a86d9e3e0db2b4c69ae
emura shigeto <shigeto@emuranoMacBook-Air.local>
2024-01-26 13:01:58 +0900

    イベント使用

--- src/App.vue
+++ src/App.vue
@@ -16,56 +16,64 @@ const iceCreamFlavors = ref([]);
 
 <template>
   <h1>{{ header }}</h1>
-  <input v-model.lazy="newItem" type="text" placeholder="Add an item" />
-  <!-- ラジオボタン -->
-  Priority:
-  <label>
-    <input type="radio" v-model="newItemPriority" value="low" />
-    Low
-  </label>
-  <label>
-    <input type="radio" v-model="newItemPriority" value="high" />
-    High
-  </label>
-  <br />
-  <!-- セレクトボックス -->
-  <label>
+  <form
+    class="add-item-form"
+    @submit.prevent="items.push({ id: items.length + 1, label: newItem })"
+  >
+    <input v-model.trim="newItem" type="text" placeholder="Add an item" />
+    <!-- ラジオボタン -->
     Priority:
-    <select v-model="newItemSelectBox">
-      <option value="low">Low</option>
-      <option value="high">High</option>
-    </select>
-  </label>
-  <br />
-  <!-- チェックボックス -->
-  <label>
-    <input type="checkbox" v-model="newItemCheckBox" />
-    High Priority
-    {{ newItemCheckBox }}
-  </label>
-  <br />
-  <!-- リスト -->
-  <label>
-    <input type="checkbox" v-model="iceCreamFlavors" value="vanilla" />Vanilla
-  </label>
-  <label>
-    <input
-      type="checkbox"
-      v-model="iceCreamFlavors"
-      value="Chocolate"
-    />Chocolate
-  </label>
-  <label>
-    <input
-      type="checkbox"
-      v-model="iceCreamFlavors"
-      value="strawberry"
-    />Strawberry
-  </label>
-  <!-- リスト -->
-  <ul>
-    <li v-for="{ id, label } in items" :key="id">
-      {{ label }}
-    </li>
-  </ul>
+    <label>
+      <input type="radio" v-model="newItemPriority" value="low" />
+      Low
+    </label>
+    <label>
+      <input type="radio" v-model="newItemPriority" value="high" />
+      High
+    </label>
+    <br />
+    <!-- セレクトボックス -->
+    <label>
+      Priority:
+      <select v-model="newItemSelectBox">
+        <option value="low">Low</option>
+        <option value="high">High</option>
+      </select>
+    </label>
+    <br />
+    <!-- チェックボックス -->
+    <label>
+      <input type="checkbox" v-model="newItemCheckBox" />
+      High Priority
+      {{ newItemCheckBox }}
+    </label>
+    <br />
+    <!-- リスト -->
+    <label>
+      <input type="checkbox" v-model="iceCreamFlavors" value="vanilla" />Vanilla
+    </label>
+    <label>
+      <input
+        type="checkbox"
+        v-model="iceCreamFlavors"
+        value="Chocolate"
+      />Chocolate
+    </label>
+    <label>
+      <input
+        type="checkbox"
+        v-model="iceCreamFlavors"
+        value="strawberry"
+      />Strawberry
+    </label>
+    <br />
+    <!-- ボタン -->
+    <button class="btn btn-primary">Save Item</button>
+    <!-- リスト -->
+    <ul>
+      <li v-for="{ id, label } in items" :key="id">
+        {{ label }}
+      </li>
+    </ul>
+  </form>
 </template>
